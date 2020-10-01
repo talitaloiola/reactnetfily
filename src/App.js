b0f6cb28790a36ece604a52795b28214fb2de7f9
Talita Loiola <talitaloiola@bol.com.br>
2020-10-01 12:09:18 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Routes from './router.js'; 
+
 
 export default function App() {
   return (
+    <Routes />
     <h1> Netlify Talita </h1>
+    
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './style.css';
+import Menu from '../Menu';
 
 export default function Home() {
   return (
+    <Menu />
     <h1> Home </h1>
   );
 }
