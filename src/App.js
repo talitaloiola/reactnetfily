b1b92256d5c323f482bec01be18e3b827fd36c1f
Talita Loiola <talitaloiola@bol.com.br>
2020-10-02 09:31:34 -0300

    Corrigindo Rotas (Return)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Routes from './router.js';
 
 export default function App() {
   return (
+    <>
     <Routes />
     <h1> Netlify Talita </h1>
-    
+    </>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,9 @@ import Menu from '../Menu';
 
 export default function Home() {
   return (
+    <>
     <Menu />
     <h1> Home </h1>
+    </>
   );
 }
